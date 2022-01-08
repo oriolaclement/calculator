664958ec71e6868d730d1e2de79fae91f7c48e8f
Oriola Timilehin <oriolatimilehin5@gmail.com>
2022-01-08 10:36:18 +0100

    my first commit

---
+++ cal_cu.py
@@ -0,0 +1,6 @@
+def calculate():
+    fir_num = float(input('Enter your first number : '))
+    sec_num = float(input('Enter your second number : '))
+    add = fir_num + sec_num
+    print(add)
+calculate()
